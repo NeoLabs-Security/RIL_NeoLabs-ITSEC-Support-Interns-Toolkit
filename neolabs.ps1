296a7c75b154bba9efacf64cc3f54217bfb0472a
khristos24 <166956265+khristos24@users.noreply.github.com>
2026-08-24 10:54:35 +0100

    Route Support login through resilient hidden prompt

--- neolabs.ps1
+++ neolabs.ps1
@@ -1,18 +1,32 @@
 $ErrorActionPreference = 'Stop'
-$env:NEOLABS_LAB_BASE_URL = 'https://pg1wb0sklb.execute-api.us-east-1.amazonaws.com'
+if ([string]::IsNullOrWhiteSpace($env:NEOLABS_LAB_BASE_URL)) {
+    $env:NEOLABS_LAB_BASE_URL = 'https://pg1wb0sklb.execute-api.us-east-1.amazonaws.com'
+}
 $client = Join-Path $PSScriptRoot 'tools\neolabs.py'
+$loginClient = Join-Path $PSScriptRoot 'tools\support_login.py'
 $ticketsClient = Join-Path $PSScriptRoot 'tools\support_tickets.py'
 if (-not (Test-Path $client)) { throw 'NeoLabs client file is missing from this toolkit.' }
 
 $scriptToRun = $client
 $forwardArgs = @($args)
-if ($args.Count -gt 0 -and $args[0].ToString().ToLowerInvariant() -eq 'tickets') {
-    if (-not (Test-Path $ticketsClient)) { throw 'NeoLabs Support ticket client is missing from this toolkit.' }
-    $scriptToRun = $ticketsClient
-    if ($args.Count -gt 1) {
-        $forwardArgs = @($args[1..($args.Count - 1)])
-    } else {
-        $forwardArgs = @()
+if ($args.Count -gt 0) {
+    $command = $args[0].ToString().ToLowerInvariant()
+    if ($command -eq 'login') {
+        if (-not (Test-Path $loginClient)) { throw 'NeoLabs Support login client is missing from this toolkit.' }
+        $scriptToRun = $loginClient
+        if ($args.Count -gt 1) {
+            $forwardArgs = @($args[1..($args.Count - 1)])
+        } else {
+            $forwardArgs = @()
+        }
+    } elseif ($command -eq 'tickets') {
+        if (-not (Test-Path $ticketsClient)) { throw 'NeoLabs Support ticket client is missing from this toolkit.' }
+        $scriptToRun = $ticketsClient
+        if ($args.Count -gt 1) {
+            $forwardArgs = @($args[1..($args.Count - 1)])
+        } else {
+            $forwardArgs = @()
+        }
     }
 }
 
